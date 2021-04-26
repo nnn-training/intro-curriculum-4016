9d5fd4ffd21a8f7c187355ae887072019554b683
taigaozawa <taigaozawa@gmail.com>
2021-04-26 21:53:57 +0900

    練習解答

--- test/test.js
+++ test/test.js
@@ -29,3 +29,12 @@ describe('/login', () => {
       .expect(200);
   });
 });
+
+describe('/logout', () => {
+  test('/ にリダイレクトされる', () => {
+    return request(app)
+      .get('/logout')
+      .expect('Location', '/')
+      .expect(302);
+  });
+});
