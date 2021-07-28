d8485e7322514fe23add2c3de9062efd16720cde
SaitoYoshiko <yo.saito3759@icloud.com>
2021-07-29 00:29:26 +0900

    練習問題に取り組みました, 少し難しかった

--- test/test.js
+++ test/test.js
@@ -5,9 +5,9 @@ const passportStub = require('passport-stub');
 
 describe('/login', () => {
   beforeAll(() => {
-       passportStub.install(app);
-       passportStub.login({ username: 'testuser' });
-   });
+    passportStub.install(app);
+    passportStub.login({ username: 'testuser' });
+  });
 
   afterAll(() => {
     passportStub.logout();
@@ -28,4 +28,15 @@ describe('/login', () => {
       .expect(/testuser/)
       .expect(200);
   });
+
+});
+
+//練習問題で追記
+describe('/logout', () => {
+  test('/logout にアクセスした際に / にリダイレクトされる', () => {
+    return request(app)
+      .get('/logout')
+      .expect('Location', '/').expect(302);
+  });
+
 });
