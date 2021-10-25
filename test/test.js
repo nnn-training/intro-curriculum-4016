5c625f88d40235b7479990c08a7ca3dc9fa0cec4
sukebon <39957486+sukebon@users.noreply.github.com>
2021-10-25 21:46:57 +0900

    【サービス開発2】プロジェクトの作成と認証の実装 課題

--- test/test.js
+++ test/test.js
@@ -1,31 +1,36 @@
-'use strict';
-const request = require('supertest');
-const app = require('../app');
-const passportStub = require('passport-stub');
+"use strict";
+const request = require("supertest");
+const app = require("../app");
+const passportStub = require("passport-stub");
 
-describe('/login', () => {
+describe("/login", () => {
   beforeAll(() => {
-       passportStub.install(app);
-       passportStub.login({ username: 'testuser' });
-   });
+    passportStub.install(app);
+    passportStub.login({ username: "testuser" });
+  });
 
   afterAll(() => {
     passportStub.logout();
     passportStub.uninstall(app);
   });
 
-  test('ログインのためのリンクが含まれる', () => {
+  test("ログインのためのリンクが含まれる", () => {
     return request(app)
-      .get('/login')
-      .expect('Content-Type', 'text/html; charset=utf-8')
+      .get("/login")
+      .expect("Content-Type", "text/html; charset=utf-8")
       .expect(/<a href="\/auth\/github"/)
       .expect(200);
   });
 
-  test('ログイン時はユーザー名が表示される', () => {
+  test("ログイン時はユーザー名が表示される", () => {
     return request(app)
-      .get('/login')
+      .get("/login")
       .expect(/testuser/)
       .expect(200);
+  });
+});
+describe("/logout", () => {
+  test("/logout にアクセスした際に / にリダイレクトされる", () => {
+    return request(app).get("/logout").expect("Location", "/").expect(302);
   });
 });
