73fc35edc63ee34b8ad89968552231ab65087f9b
Kenichi Oosaki <kenichi_oosaki@dwango.co.jp>
2022-04-29 14:40:44 +0900

    async/await対応

--- test/test.js
+++ test/test.js
@@ -5,25 +5,25 @@ const passportStub = require('passport-stub');
 
 describe('/login', () => {
   beforeAll(() => {
-       passportStub.install(app);
-       passportStub.login({ username: 'testuser' });
-   });
+    passportStub.install(app);
+    passportStub.login({ username: 'testuser' });
+  });
 
   afterAll(() => {
     passportStub.logout();
     passportStub.uninstall(app);
   });
 
-  test('ログインのためのリンクが含まれる', () => {
-    return request(app)
+  test('ログインのためのリンクが含まれる', async () => {
+    await request(app)
       .get('/login')
       .expect('Content-Type', 'text/html; charset=utf-8')
       .expect(/<a href="\/auth\/github"/)
       .expect(200);
   });
 
-  test('ログイン時はユーザー名が表示される', () => {
-    return request(app)
+  test('ログイン時はユーザー名が表示される', async () => {
+    await request(app)
       .get('/login')
       .expect(/testuser/)
       .expect(200);
