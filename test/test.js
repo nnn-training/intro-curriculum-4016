311630dec07abc2eb25998eaa0e5faa1edd51eaf
enpitsu882 <enpitsu882@hotmail.co.jp>
2023-04-06 20:24:16 +0900

    ログアウトテストの追加

--- test/test.js
+++ test/test.js
@@ -29,3 +29,12 @@ describe('/login', () => {
       .expect(200);
   });
 });
+
+describe('/logout', () => {
+  test('ログアウトした後にリダイレクトされる', async () => {
+    await request(app)
+      .get('/logout')
+      .expect('Location', '/')
+      .expect(302);
+  });
+})
