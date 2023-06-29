fd8c06e418546969fe6f5212cd241e8b3f589db4
kawasakinwwolf <kawasaki900rninja@gmail.com>
2023-06-29 22:56:29 +0900

    ログアウト時のリダイレクトのテスト実装

--- test/test.js
+++ test/test.js
@@ -29,3 +29,13 @@ describe('/login', () => {
       .expect(200);
   });
 });
+
+
+describe('/logout', () => {
+  test('ログアウト時にトップページへリダイレクトする', async () => {
+    await request(app)
+    .get('/logout')
+    .expect('Location', '/')
+    .expect(302);
+  });
+});
